de9d4ba12b5bae9197c59a72eba4729b4157fb6a
danie <daniel.freitas.ms1@gmail.com>
2020-05-25 23:03:10 -0300

    added query option

--- src/main/java/br/com/restwithmongodb/demo/Models/Repositories/PostRepository.java
+++ src/main/java/br/com/restwithmongodb/demo/Models/Repositories/PostRepository.java
@@ -2,9 +2,13 @@ package br.com.restwithmongodb.demo.Models.Repositories;
 
 import br.com.restwithmongodb.demo.Models.Entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle (String title);
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
